c16f5eb5fa08ef5b1cb83e9657368db23ca4b12c
MisterVladimir <vladimir.shteyn@googlemail.com>
2018-02-08 17:15:02 -0500

    Query
    Query brought to what I think is final form, but still untested.

--- pubmed-scraping/query.py
+++ pubmed-scraping/query.py
@@ -16,17 +16,14 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import requests 
-import csv
 import numpy as np
 from collections import OrderedDict
 from abc import ABC, abstractmethod
 import pickle
-
-from soups import SimpleUIDList
-
+import h5py
 
 class Query(ABC): 
     """
@@ -38,73 +35,135 @@ class Query(ABC):
     
     class _SearchTerms(object): 
         def __init__(self, arg): 
+            """
+            Converts the arg into appropriate format. Should take iterable 
+            or Soup object from which search terms e.g. Pubmed IDs can be 
+            parsed. 
+            """
             raise NotImplementedError('Implement in a child class.') 
     
+    def _load_h5(self, path): 
+        raise NotImplementedError('Not implemented.')  
+            
+    def _save_h5(self, path): 
+        raise NotImplementedError('Not implemented.')  
+    
     def _to_url(self, base): 
-        query = self.fields.copy()
+        query = self.fields
         query[self.query_key] = self.search_terms.to_url()
         
-        return base + ''.join(['{0}={1}&'.format(k, v) for \
-                               k, v in query.items()])[:-1] 
+        url = base + '&'.join(['{0}={1}'.format(k, v) for k, v in query.items()])
+        return url
+    
     @abstractmethod
     def to_url(self): 
         raise NotImplementedError('Implement in a child class.') 
+    
+    def load(self, terms=None, path=None): 
+        """
+        Instantiates a _SearchTerms object directly from the passed-in has_uids
+        instance or loads UIDs from file and converts them to _SearchTerms 
+        object. 
+        
+        Parameters
+        ------------
+        has_uids : SimpleUIDList
+            Anything that has a 'uid' attribute. This is parsed into a list 
+            of integers. 
+        
+        path: str
+            Path to file. 
+            
+        """ 
+        if not terms is None: 
+            self.search_terms = self._SearchTerms(terms) 
+        elif not path is None: 
+            extension = path.split('.')[-1]
+            if extension in ('h5', 'hd5', 'hf5'): 
+                result = self._load_h5(path)
+                    
+            elif extension in ('txt', 'pickle', 'pkl'): 
+                read = None 
+                with open(path, 'rb') as f: 
+                    if extension == 'txt': 
+                        read = f.read().decode('utf-8')
+                    elif extension in ('pickle', 'pkl'): 
+                        read = pickle.load(f, encoding='utf-8')
+                        
+                result = [line.split(',') for line in read.split('\n')] 
+            
+            self.search_terms = self._SearchTerms(result) 
+    
+    def save(self, path): 
+#        TODO: save to file from which UIDs were loaded 
+        extension = path.split('.')[-1] 
+        if extension == 'txt': 
+            with open(path, 'wb') as f: 
+                f.write(self.search_terms.saveable_format)
+        elif extension in ('pickle', 'pkl'): 
+            with open(path, 'wb') as f: 
+                pickle.dump(self.search_terms.saveable_format, f)
+        elif extension in ('h5', 'hd5', 'hf5'): 
+            self._save_h5(path) 
+
+        else: 
+            raise IOError('Incompatible file type; use one of {0}.'.format(
+                                    ', '.join(self.extensions)) 
+                                                                           )
+    
 
 class KeyWordQuery(Query): 
-    query_key = 'entry'
-    kw_conversion = {'':'', 'title':'ti', 'journal':'journal', 
-                     'affiliation':'affiliation'} 
-    base_url = r"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
+    query_key = 'term'
+    base_url = r"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?"
     fields = OrderedDict([('db', 'pubmed'), 
                           (query_key, '')]) 
     soup_sources = None
     
     class _SearchTerms(dict): 
+        kw_conversion = {'':'', ' ':'', 'title':'ti', 'journal':'journal', 
+                         'affiliation':'affiliation'} 
         def __init__(self, arg): 
-            arg = filter(lambda k: k[0] in KeyWordQuery.kw_conversion.keys(), arg.items())
+            arg = {li[0]: li[1:] for li in arg if \
+                                            li[0] in self.kw_conversion.keys()}
             super().__init__(arg)
         
         def to_url(self): 
 #            TODO: boolean logic in search 
-            url = '&term='
-            for k in KeyWordQuery.query_fields: 
-                url += '+'.join([str(self[k])+'[{0}]'.format(KeyWordQuery.kw_conversion[k])]) + '+'
-            return url[:-1]
+            url = ''
+            for k, v in self.items(): 
+                url += '+'.join([self[v]+'[{0}]'.format(self.kw_conversion[k])])
+            return url
         
-        def save(self, path): 
-            with open(path, 'w', newline='') as f: 
-                writer = csv.writer(f, delimiter=",", quoting=csv.QUOTE_NONE)
-                for k, v in self.search_terms.items(): 
-                    writer.writerow([k] + v) 
-        
-    def __init__(self): 
-        super().__init__()
+        @property
+        def saveable_format(self): 
+            li = [k + ',' + ','.join(v) for k, v in self.items()]
+            return '\n'.join(li).encode('utf-8')
     
-    def load(self, items=None, path=None): 
-        """
-        Load query entries from a dictionary, csv or text file. 
-        """
-        if not items is None: 
-            self.search_terms = self._SearchTerms(items) 
-        elif path is None: 
-            terms = {} 
-            extension = path.split('.')[-1] 
-            if extension == 'csv': 
-                with open(path, 'r', newline='') as f: 
-                    reader = csv.reader(f, quoting=csv.QUOTE_NONE) 
-                    for line in reader: 
-                        terms[line[0]] = line[1:]
+    def _save_h5(self, path): 
+        with h5py.File(path, mode='w') as f: 
+            for k, v in self.fields.items(): 
+                f.attrs[k] = v.encode('utf-8')
+            for grp, val in f.items(): 
+                f.create_dataset(grp.name, np.array(val, dtype=bytes))
             
-                self.search_terms = self._SearchTerms(terms) 
-            
-            elif extension == 'txt': 
-                pass 
-    
-    def save(self, path): 
-        self.search_terms.save(path) 
+    def _load_h5(self, path): 
+        terms = [] 
+        with h5py.File(path, 'r') as f: 
+            for k, v in f.attrs.items(): 
+                if k in self.fields.keys(): 
+                    self.fields[k] = v.decode('utf-8')
+            li = []
+            for grp, val in f.items(): 
+                li.append([grp.name] + list(map(lambda x: x.decode('utf-8'), 
+                                                val)))
+        return terms 
     
     def to_url(self): 
-        pass 
+        base = self.base_url
+        req_function = requests.get
+        
+        return self._to_url(base), req_function
+
 
 class UIDQuery(Query): 
     """
@@ -124,34 +183,41 @@ class UIDQuery(Query):
     searchable_db = ['pubmed']
     
     class _SearchTerms(list): 
-        extensions = ['txt', 'csv', 'pickle', 'pkl']
-        def __init__(self, arg): 
-            arg = map(str, arg.uid)
-            super().__init__(arg)
+        def __init__(self, terms): 
+            try: 
+                terms = terms.uid
+            except AttributeError: 
+                pass 
+            terms = filter(lambda x: x.isdigit(), 
+                           map(lambda x: str(x).replace(' ', ''), 
+                               np.concatenate(terms)))
+
+            super().__init__(terms)
         
         def to_url(self): 
-            return ','.join(self)
+            return ','.join(self) 
         
-        def save(self, path): 
-            extension = path.split('.')[-1] 
-            if extension == 'csv': 
-                with open(path, 'w', newline='') as f: 
-                    writer = csv.writer(f, delimiter=",", quoting=csv.QUOTE_NONE)
-                    writer.writerow(self)  
-            elif extension == 'txt': 
-                with open(path, 'wb', newline='') as f: 
-                    f.write(','.join(self).encode('utf-8'))
-            elif extension in ('pickle', 'pkl'): 
-                with open(path, 'wb', newline='') as f: 
-                    pickle.dump(','.join(self).encode('utf-8'), f)
-            else: 
-                raise IOError('Incompatible file type; use one of {0}.'.format(
-                                        ', '.join(self.extensions)) 
-                                                                               )
+        @property
+        def saveable_format(self): 
+            return ','.join(self).encode('utf-8')
+    
+    def _save_h5(self, path): 
+        with h5py.File(path, mode='w') as f: 
+            fields = self.fields.copy() 
+            del fields[self.query_key]
+            for k, v in fields.items(): 
+                f.attrs[k] = v.encode('utf-8')
+            f.create_dataset('uid',data=np.array(self.search_terms, dtype=int)) 
             
-    def __init__(self): 
-        super().__init__()
-     
+    def _load_h5(self, path): 
+        terms = [] 
+        with h5py.File(path, 'r') as f: 
+            for k, v in f.attrs.items(): 
+                if k in self.fields.keys(): 
+                    self.fields[k] = v.decode('utf-8')
+            terms = f['uid']
+        return terms 
+    
     @property
     def database(self): 
         return self.fields['db'] 
@@ -172,53 +238,13 @@ class UIDQuery(Query):
         if val >= 1e5: #NCBI can't retreive more than 1e5 UIDs
             val = 1e5
         self.fields['retmax'] = str(val)
-        
-    
-    def load(self, has_uids=None, path=None): 
-        """
-        Instantiates a _SearchTerms object directly from the passed-in has_uids
-        instance or loads UIDs from file and converts them to _SearchTerms 
-        object. 
-        
-        Parameters
-        ------------
-        has_uids : SimpleUIDList
-            Anything that has a 'uid' attribute. This is parsed into a list 
-            of integers. 
-        
-        path: str
-            Path to file. 
-            
-        """
-        if not has_uids is None: 
-            self.search_terms = self._SearchTerms(has_uids) 
-        elif not path is None: 
-            terms = [] 
-            extension = path.split('.')[-1] 
-            with open(path, 'rb') as f: 
-                if extension == 'csv': 
-                    reader = csv.reader(f, quoting=csv.QUOTE_NONE) 
-                    for line in reader: 
-                        terms.append(line) 
-            
-                elif extension == 'txt': 
-                    terms = f.read().decode('utf-8').replace(' ', '').split(',')
-            
-                elif extension in ('pickle', 'pkl'): 
-                    p = pickle.load(f, encoding='utf-8')
-                    terms = p.split(',')
-                    
-            terms = SimpleUIDList(filter(lambda x: x.isdigit(), 
-                                         map(str, np.concatenate(terms))))
-            self.search_terms = self._SearchTerms(terms) 
-    
-    def save(self, path): 
-        self.search_terms.save(path)
     
     def to_url(self): 
         if len(self.fields['id']) >= 200: 
             base = self.base_url['high'] 
-            return self._to_url(base), requests.get
+            req_function = requests.get
         else: 
             base = self.base_url['low'] 
-            return self._to_url(base), requests.post
+            req_function = requests.post
+
+        return self._to_url(base), req_function
